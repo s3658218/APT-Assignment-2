62a1a721a4bc86ad882e6c2aee937d35828d62bc
Tylar <s3604139@student.rmit.edu.au>
2019-05-02 13:56:18 +1000

    Pulling Geroge's Branch

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # APT-Assignmnent-2
 A repository for our Assignment 2.
+<<<<<<< HEAD
 First commit for branch
+=======
+
+George Korovinis has commited this, test
+>>>>>>> f504098c6055b256fb059e7589b393966addce7d
--- starter_code/qwirkle.cpp
+++ starter_code/qwirkle.cpp
@@ -5,11 +5,37 @@
 
 #define EXIT_SUCCESS    0
 
+//declaring proto-type function
+void mainMenu();
+
 int main(void) {
    LinkedList* list = new LinkedList();
    delete list;
 
    std::cout << "TODO: Implement Qwirkle!" << std::endl;
 
+   //calling mainMenu function when program starts
+   mainMenu();
+
    return EXIT_SUCCESS;
+}
+
+public void mainMenu()
+void mainMenu()
+{
+  std::cout << "Welcome to the jungle!" << std::endl;
+  std::cout << "Welcome to Qwirkle!" << std::endl;
+  std::cout << "-------------------" << std::endl;
+  std::cout << "\n";
+  std::cout << "Main Menu" << std::endl;
+  std::cout << "---------" << std::endl;
+
+  std::cout << "1. New Game " << std::endl;
+  std::cout << "2. Load Game " << std::endl;
+  std::cout << "3. Show student information " << std::endl;
+  std::cout << "4. Exit " << std::endl;
+  std::cout << "\n";
+  
+  // test from PC
+
 }
