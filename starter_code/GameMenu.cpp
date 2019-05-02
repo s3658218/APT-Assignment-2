3e4c48cbfcc66573433f8ec30991624812ea9b33
lvossos <lvossos210@gmail.com>
2019-05-02 15:09:53 +1000

    Created GameMenu.cpp
    - created GameMenu.cpp
    - Modified qwirkle.cpp

---
+++ starter_code/GameMenu.cpp
@@ -0,0 +1,19 @@
+  #include <iostream>
+
+  //declare prototype functions
+
+  void newGame()
+  {
+    std::cout << "Starting a New Game " << std::endl;
+  }
+
+
+  void loadGame()
+  {
+
+  }
+
+  void studentInfo()
+  {
+
+  }
--- starter_code/qwirkle.cpp
+++ starter_code/qwirkle.cpp
@@ -1,5 +1,6 @@
 
 #include "LinkedList.h"
+#include "GameMenu.cpp"
 
 #include <iostream>
 
@@ -26,42 +27,44 @@ int main(void)
 
 void mainMenu()
 {
-  std::cout << "Welcome to Qwirkle!" << std::endl;
-  std::cout << "-------------------" << std::endl;
-  std::cout << "\n";
-  std::cout << "Main Menu" << std::endl;
-  std::cout << "---------" << std::endl;
-
-  std::cout << "1. New Game " << std::endl;
-  std::cout << "2. Load Game " << std::endl;
-  std::cout << "3. Show student information " << std::endl;
-  std::cout << "4. Exit " << std::endl;
-  std::cout << "\n";
-
-  //read playerInput
-  std::cout << "Enter: ";
-  std::cin >> playerInput;
 
   //create do-while loop for user input
   do
   {
+    std::cout << "Welcome to Qwirkle!" << std::endl;
+    std::cout << "-------------------" << std::endl;
+    std::cout << "\n";
+    std::cout << "Main Menu" << std::endl;
+    std::cout << "---------" << std::endl;
+
+    std::cout << "1. New Game " << std::endl;
+    std::cout << "2. Load Game " << std::endl;
+    std::cout << "3. Show student information " << std::endl;
+    std::cout << "4. Exit " << std::endl;
+    std::cout << "\n";
+
+    //read playerInput
+    std::cout << "Enter Input: ";
+    std::cin >> playerInput;
+
     if(playerInput = 1)
     {
-
+      newGame();
     }
     else if(playerInput = 2)
     {
-
+      loadGame();
     }
     else if(playerInput = 3)
     {
-
+      studentInfo();
     }
     else
     {
-
+      std::cout << "Enter a valid response from menu" << std::endl;
     }
   }
   while(playerInput != 4);
+
 
 }
