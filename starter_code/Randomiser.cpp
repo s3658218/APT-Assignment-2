17c3741c9067ecb522d84edcf0bde8be3fc42eb6
Tylar <s3604139@student.rmit.edu.au>
2019-05-09 13:52:31 +1000

    commented on randomiser code

--- starter_code/Randomiser.cpp
+++ starter_code/Randomiser.cpp
@@ -1,18 +1,21 @@
 #define NULL 0
 #include <iostream>
+#include <random>
 using std::cout;
-
+// Node class
 class Node{
     public:
     int data;
     Node* next;
 };
+//Function that will iterate through the loop and print the values
 void printList(Node *n){
     while (n != NULL){
         cout << n->data << "";
         n = n -> next;
     }
 }
+//Assigning values to the linked list
 int main() {
     Node* head = NULL;
     Node* second = NULL;
@@ -28,8 +31,10 @@ int main() {
     second -> next = third;
     third -> data = 3;
     third -> next = NULL;
+    //Calling the print function with head being the first n value
     printList(head);
     return 0;
 }
 void shuffleBag(Node *n){
+
 }
