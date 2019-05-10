a23b48d4661ca018f30b740196d1eb330fecdc26
George Korovinis <s3658218@student.rmit.edu.au>
2019-05-10 21:14:07 +1000

    fix

--- starter_code/qwirkle.cpp
+++ starter_code/qwirkle.cpp
@@ -14,16 +14,8 @@ using std::endl;
 //declaring proto-type function
 void mainMenu();
 
-//global vars
-//int playerInput = 0;
-
 int main(void)
 {
-   //LinkedList* list = new LinkedList();
-   //delete list;
-
-   //std::cout << "TODO: Implement Qwirkle!" << std::endl;
-
    //calling mainMenu function when program starts
    mainMenu();
 
