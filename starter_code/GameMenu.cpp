a286149dbb682536aa22c5e70c5802440a66a809
LorenzoVee <49382390+LorenzoVee@users.noreply.github.com>
2019-05-12 12:55:33 +1000

    Added Georges Code...
    - GameMenu.cpp
    - GameMenu.h
    - qwirkle.exe

--- starter_code/GameMenu.cpp
+++ starter_code/GameMenu.cpp
@@ -1,32 +1,96 @@
   #include <iostream>
-  //include player file
-  #include "Player.h"
+  #include <string>
+  #include "GameMenu.h"
 
   using std::string;
   using std::cout;
   using std::endl;
+  using std::cin;
 
-  //declare prototype functions
+int playerInput;
+std::string player1;
+std::string player2;
+bool endLoop;
 
-  void newGame()
-  {
-    //string player1 = "";
-    //string player2 = "";
-
-    std::cout << "Starting a New Game " << std::endl;
-
-    std::cout << "Enter a name for player 1 " << std::endl;
-    //std::cin >> player1;
-
-  }
-
-
-  void loadGame()
+void mainMenu()
+{
+  do
   {
 
-  }
+    cout << endl;
+    cout << "Welcome to Qwirkle!" << std::endl;
+    cout << "-------------------" << std::endl;
+    cout << endl;
+    cout << "Main Menu" << endl;
+    cout << "---------" << endl;
 
-  void studentInfo()
-  {
+    cout << "1. New Game " << endl;
+    cout << "2. Load Game " << endl;
+    cout << "3. Show student information " << endl;
+    cout << "4. Exit " << endl;
+    cout << endl;
 
+    //read playerInput
+    cout << "Enter Input: ";
+    cin >> playerInput;
+
+    if(playerInput == 1)
+    {
+      newGame();
+    }
+    else if(playerInput == 2)
+    {
+      loadGame();
+    }
+    else if(playerInput == 3)
+    {
+      studentInfo();
+    }
+    else if (playerInput == 4)
+    {
+      exitProgram();
+    } else {
+      cout << endl;
+      cout << "~~~PLEASE ENTER A CORRECT VALUE!~~~" << endl;
+    }
   }
+  while(endLoop == false);
+
+}
+
+void newGame() { // need to do: validation check for numbers or symbols
+  cout << "Starting a New Game\n" << endl;
+  cout << "Enter a name for player 1 (uppercase characters only)" << endl;
+  cin >> player1;
+  cout << "Enter a name for Player 2 (uppercase characters only)" << endl;
+  cin >> player2;
+  cout << endl;
+  cout << "Player 1 is: " << player1 << endl;
+  cout << "Player 2 is: " << player2 << endl;
+  cout << "Let's Play!";
+}
+
+void loadGame() {
+  cout << "Enter the filename from which load a game" << endl;
+}
+
+void studentInfo() {
+    cout << "Name: George Korovinis" << endl;
+    cout << "Student ID: s3658218" << endl;
+    cout << "Email: s3658218@student.rmit.edu.au\n" << endl;
+
+    cout << "Name: Tylar Lariba-Taing" << endl;
+    cout << "Student ID: s3604139" << endl;
+    cout << "Email: s3604139@student.rmit.edu.au\n" << endl;
+
+    cout << "Name: Lorenzo Vossos" << endl;
+    cout << "Student ID: s3792797" << endl;
+    cout << "Email: s3792797@student.rmit.edu.au\n" << endl;
+    cout << endl;
+
+}
+
+void exitProgram() {
+  cout << "Goodbye" << endl;
+  endLoop = true;
+}
---
+++ starter_code/GameMenu.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#ifndef GAMEMENU_H
+#define GAMEMENU_H
+
+void mainMenu();
+void newGame();
+void loadGame();
+void studentInfo();
+void exitProgram();
+
+#endif
