e0405177001027804c4c78dfe05e9169dd2e0c83
George Korovinis <s3658218@student.rmit.edu.au>
2019-05-19 16:47:07 +1000

    removing file

---
+++ starter_code/GameGameplay.cpp
@@ -0,0 +1,23 @@
+  #include <iostream>
+  #include "GameMenu.h"
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+
+bool endLootTurn;
+
+void completeTurn() {
+
+  do
+  {
+    cout << endl;
+    cout << "Current Player: " << player1
+  } 
+  while(endLootTurn == false);
+
+
+
+
+}
