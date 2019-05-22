ee32c4da6df2bcaaae5396c3e10706b53a27b682
s3658218 <38154453+s3658218@users.noreply.github.com>
2019-05-22 20:44:06 +1000

    Update Board.cpp

--- starter_code/Board.cpp
+++ starter_code/Board.cpp
@@ -35,9 +35,9 @@ string testArray[6][6] = {  {"  ", "  ", "  ", "  ", "  ", "  "},
 string letters[6] = {"A", "B", "C", "D", "E", "F"};
 
 void Board::displayBoard() {
-  cout << "  0  1  2  3  4  5" << endl;
+  cout << "   0   1   2   3   4   5" << endl;
   for (unsigned int i = 0; i < 6; i++) {
-    cout << letters[i];
+    cout << letters[i] << " ";
     //cout << endl;
       for (unsigned int j = 0; j < 6; j++) {
           cout << "|" <<testArray[i][j] << "|";
@@ -50,21 +50,21 @@ void Board::displayBoard() {
 
 
 void Board::testBoard() {
-  while (tileCheck != true){
+  //while (tileCheck != true){
   cout << "Which colour would you like to place" << endl;
   cin >> x;
   cout << "Which shape would you like to place" << endl;
   cin >> y;
-  Tile* tmpTile = new Tile(x, y);
-  ll.tileComparePlace(gm.z, tmpTile, tileCheck);
+  //Tile* tmpTile = new Tile(x, y);
+  //ll.tileComparePlace(gm.z, tmpTile, tileCheck);
   string placeholderXD;
   string placeholder1 = string(1, x);
   string placeholder2;
   placeholder2 = std::to_string(y);
-  cout << placeholder1 << endl;
-  cout << placeholder2 << endl;
+  //cout << placeholder1 << endl;
+  //cout << placeholder2 << endl;
   tile2d =  placeholder1 + placeholder2;
-}
+//}
 
   cout << "Okay! and what is the X coordinate? (starts at 0, so 0-23)" << endl;
   cin >> tileLocationX;
@@ -86,4 +86,3 @@ void Board::testBoard() {
   //}
   cout << endl;
 }
-//:)
